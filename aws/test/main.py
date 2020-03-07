bc470b5af6fcc01544d388c0bf2e4714d10dfe2a
Neelansh Mathur <neelansh15@outlook.com>
2020-03-07 15:28:15 +0530

    Added Lists of Entities. And the highest scoring organization is selected now.

--- aws/test/main.py
+++ aws/test/main.py
@@ -2,7 +2,7 @@ import boto3
 
 # Document
 s3BucketName = "invoice-storage-unifyed"
-documentName = "Croma (7).jpg"
+documentName = "Screenshot20200307135515ScanbotSDKExampleFlutterpng"
 
 # Amazon Textract client
 textract = boto3.client('textract')
@@ -19,22 +19,61 @@ response = textract.detect_document_text(
 #print(response)
 
 # Print text
-print("\nText\n========")
+# print("\nText\n========")
 text = ""
 for item in response["Blocks"]:
     if item["BlockType"] == "LINE":
-        print ('\033[94m' +  item["Text"] + '\033[0m')
+        # print ('\033[94m' +  item["Text"] + '\033[0m')
         text = text + " " + item["Text"]
 
 # Amazon Comprehend client
 comprehend = boto3.client('comprehend')
 
-# Detect sentiment
-sentiment =  comprehend.detect_sentiment(LanguageCode="en", Text=text)
-print ("\nSentiment\n========\n{}".format(sentiment.get('Sentiment')))
-
 # Detect entities
+titles = []
+organization = ""
+other_orgs = []
+org_highest = 0
+locations = []
+quantities = []
+other = []
+date = []
+people = []
+commercial_items = []
+
+
 entities =  comprehend.detect_entities(LanguageCode="en", Text=text)
 print("\nEntities\n========")
 for entity in entities["Entities"]:
-    print ("{}\t=>\t{}".format(entity["Type"], entity["Text"]))
+    # print ("{}\t=>\t{}".format(entity["Type"], entity["Text"]))
+    if entity["Type"] == "ORGANIZATION":
+
+        # Include only the organization with the highest score
+        if entity["Score"] > org_highest:
+            org_highest = entity["Score"]
+            organization = entity["Text"]
+        
+        if entity["Score"] > 0.5:
+            other_orgs.append(entity["Text"])
+    
+    elif entity["Type"] == "LOCATION":
+        if entity["Score"] > 30:
+            locations.append(entity["Text"])
+    elif entity["Type"] == "TITLE":
+        titles.append(entity["Text"])
+    elif entity["Type"] == "QUANTITY":
+        quantities.append(entity["Text"])
+    elif entity["Type"] == "OTHER":
+        other.append(entity["Text"])
+    elif entity["Type"] == "DATE":
+        date.append(entity["Text"])
+    elif entity["Type"] == "PERSON":
+        people.append(entity["Text"])
+    elif entity["Type"] == "COMMERCIAL_ITEM":
+        commercial_items.append(entity["Text"])
+    
+
+print()
+print(organization)
+print("Location: \n".join(locations))
+
