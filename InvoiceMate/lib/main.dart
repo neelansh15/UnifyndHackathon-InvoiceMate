1698c0012b21d7291e8d20e0925475cf6afc3020
Techno-Disaster <nitinnirve@gmail.com>
2020-03-07 16:11:55 +0530

    App done ig?

---
+++ InvoiceMate/lib/main.dart
@@ -0,0 +1,19 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './screens/scanner.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "InvoiceMate",
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MenuDashboardPage(),
+      },
+    ),
+  );
+}
